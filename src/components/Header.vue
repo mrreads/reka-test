5c195c705f32f55f23edfee46d1bd0755e688418
Alexander <mrreads@yandex.com>
2022-11-01 15:31:06 +0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="header">
+        <div class="header__content">
+            <h1> <span>Выберите</span> вашу новую яхту </h1>
+            <div class="header__content-line" />
+            <p>Ищите, сравнивайте и покупайте лодки с помощью нашего каталого и проверенных экспертов</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &__content {
+        display: flex;
+        flex-flow: column nowrap;
+
+        & h1 {
+            font-family: "Cormorant";
+            font-size: 62px;
+            text-transform: uppercase;
+        }
+
+        & p {
+            font-family: "Roboto";
+            font-size: 16px;
+        }
+
+        & h1, p {
+            color: #FFFFFF;
+            text-align: center;
+            z-index: 1;
+        }
+
+        & span {
+            color: #8E9FBC;
+        }
+
+        &-line {
+            background-color: #FFFFFF;
+            opacity: 0.35;
+            height: 1px;
+            width: 100%;
+
+            margin: 30px 0;
+        }
+    }
+    
+    min-height: 715px;
+    background-image: url('@/assets/images/header_background.png');
+    background-position: center;
+    background-size: cover;
+
+    position: relative;
+    &::before {
+        position: absolute;
+        content: '';
+        top: 0;
+        right: 0;
+        width: 100%;
+        height: 100%;
+        background-color: #22282dd0;
+    }
+    &::after {
+        content: "yachting";
+
+        position: absolute;
+        left: 47px;
+        bottom: 43px;
+
+        font-family: 'Roboto';
+        font-size: 12px;
+        line-height: 28px;
+        letter-spacing: 12px;
+        text-transform: uppercase;
+
+        color: #FFFFFF;
+    }
+}
+</style>
