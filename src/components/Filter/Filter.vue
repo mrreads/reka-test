c0dd4d6330be73278f322ba18a42535f79e54758
Alexander <mrreads@yandex.com>
2022-11-05 18:19:03 +0300

    slider

--- src/components/Filter/Filter.vue
+++ src/components/Filter/Filter.vue
@@ -6,6 +6,7 @@ export default {
     components: { SelectFilter },
     data() {
         return { 
+            sliderValue: [0, 44231321],
             selectedWallet: 'dollar',
             type: ['Любой', 'Другой', 'Иной'], 
             category:  ['Категория 1', 'Категория 2', 'Категория 3', 'Категория 4', 'Категория 5'], 
@@ -29,14 +30,20 @@ export default {
 
             <div class="filter-slider">
                 <p class="filter-slider__title">Цена</p>
+
                 <div class="filter-slider-values">
-                    <p class="filter-slider-values__slider">от <span>0</span> </p>
-                    <p class="filter-slider-values__slider">до <span>44 234 321</span> </p>
+                    <p class="filter-slider-values__slider">от <span> {{ sliderValue[0].toLocaleString().replace(',', ' ') }} </span> </p>
+                    <p class="filter-slider-values__slider" style="margin-left: auto;">до <span> {{ sliderValue[1].toLocaleString().replace(',', ' ') }} </span> </p>
                     
                     <p @click="(selectedWallet = 'dollar')" :class="this.selectedWallet == 'dollar' ? 'filter-slider-values__wallet active' : 'filter-slider-values__wallet'">$</p>
                     <p @click="(selectedWallet = 'ruble')" :class="this.selectedWallet == 'ruble' ? 'filter-slider-values__wallet active' : 'filter-slider-values__wallet'">₽</p>
                     <p @click="(selectedWallet = 'euro')" :class="this.selectedWallet == 'euro' ? 'filter-slider-values__wallet active' : 'filter-slider-values__wallet'">€</p>
                 </div>
+
+                <div class="filter-slider__item">
+                    <vue-slider :min="0" :max="44231321" v-model="sliderValue" :tooltip="'none'"></vue-slider>
+                </div>
+                
             </div>
 
             <div class="filter-button">
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue';
-import './style.scss';
+import '@/style.scss';
+
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/default.css'
 
 import App from './App.vue';
 const app = createApp(App);
+app.component('VueSlider', VueSlider);
 app.mount('#app');
