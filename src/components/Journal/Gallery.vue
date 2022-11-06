340c37555698036d397aba04cdf806c9f49a9ff5
Alexander <mrreads@yandex.com>
2022-11-06 16:18:48 +0300

    sliders ready

--- src/components/Journal/Gallery.vue
+++ src/components/Journal/Gallery.vue
@@ -3,10 +3,12 @@ import './gallery.scss'
 
 export default {
     data() {
-        return { sliderBigTimer: 0, sliderBigImageCount: 100 }
+        return { sliderBigTimer: 0, sliderBigImageCount: 100, sliderSideImageCount: 100 }
     },
     created() {
         this.updateImage(this.$refs.bigSliderImage, this.sliderBigImageCount);
+        this.updateImage(this.$refs.sideSliderImage, this.sliderSideImageCount);
+
         setInterval(() => {
             this.sliderBigTimer += 1;
             if (this.sliderBigTimer > 99)
@@ -38,6 +40,14 @@ export default {
             this.sliderBigImageCount++, 
             this.updateImage(this.$refs.bigSliderImage, this.sliderBigImageCount);
             this.sliderBigTimer = 0;
+        },
+        prevSide() {
+            this.sliderSideImageCount--, 
+            this.updateImage(this.$refs.sideSliderImage, this.sliderSideImageCount);
+        },
+        nextSide() {
+            this.sliderSideImageCount++, 
+            this.updateImage(this.$refs.sideSliderImage, this.sliderSideImageCount);
         }
     },
 }
@@ -63,11 +73,29 @@ export default {
                     <img class="gallery-sliders__big-image" src="https://via.placeholder.com/600/92c952" ref="bigSliderImage" />
                     <div class="gallery-sliders__big-text"> В линейке лодок норвежской компании Marex Boats появилась новая модель семейного круизера, которую широкой публике покажут только в следующем году. Но мы кое-что о ней...</div>
                 </div>
-                <div class="gallery-sliders__side"></div>
+                <div class="gallery-sliders__side">
+
+                    <p class="gallery-sliders__side-text__tag"># лица индустрии</p>
+                    <p class="gallery-sliders__side-text__title">Франсис Лапп</p>
+
+                    <div class="gallery-sliders__side-arrow">
+                        <div class="gallery-sliders__side-arrow__left" @click="prevSide"><img src="@/assets/images/arrow.svg" /></div>
+                        <div class="gallery-sliders__side-arrow__right" @click="nextSide"><img src="@/assets/images/arrow.svg" /></div>
+                    </div>
+                    
+
+                    <img class="gallery-sliders__side-image" src="https://via.placeholder.com/600/DD1111" ref="sideSliderImage" />
+                    <div class="gallery-sliders__side-text"> Франсис Лапп — о том, как верфь Sunreef Yachts развивает сегмент роскошных катамаранов...</div>
+               
+                </div>
             </div>
 
             <div class="gallery-news">
-                <div class="gallery-news__side"></div>
+                <div class="gallery-news__side">
+                    <p class="gallery-news__side-text__tag"># лица индустрии</p>
+                    <img class="gallery-news__side-image" src="https://via.placeholder.com/600/123456" />
+                    <div class="gallery-news__side-text"> Марианна Хендрикс — о том, что сейчас происходит на верфи Moonen и в каком направлении она движется после смены владельцев</div>
+                </div>
                 <div class="gallery-news__item"></div>
                 <div class="gallery-news__item"></div>
             </div>
